4d1f0d9443557619f88cb1bae057e2c0ade51854
runquanye <yer@mail.gvsu.edu>
2020-01-30 16:39:23 -0500

    Initial Commit

---
+++ Homework3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework3
+//
+//  Created by Xcode User on 1/30/20.
+//  Copyright © 2020 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
