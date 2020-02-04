24d900d6e962632aac621763c8f5549b39ebeeab
runquanye <yer@mail.gvsu.edu>
2020-02-04 17:48:52 -0500

    Bananas

--- Homework3/ViewController.swift
+++ Homework3/ViewController.swift
@@ -11,16 +11,36 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var toTextField: UITextField!
+    @IBOutlet weak var fromTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
- 
-    @IBAction func calculateBtn(_ sender: Any) {
-        toTextField.text = "430 praise it"
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+         fromTextField.endEditing(true)
+         toTextField.endEditing(true)
+        
     }
+ 
 
+    @IBAction func calculateBtn(_ sender: Any) {
+        
+       let  str = fromTextField.text
+        if let input = Double(str!){
+            toTextField.textColor = UIColor.black
+            let h = 0.9144
+            let output = Double(h*input)
+            toTextField.text = String(output)
+        } else{
+            toTextField.text = "You broke it, great."
+            toTextField.textColor = UIColor.red
+        }
+
+    
+    }
     @IBAction func clearBtn(_ sender: Any) {
+        fromTextField.text = ""
+        toTextField.text = ""
     }
     @IBAction func modeBtn(_ sender: Any) {
     }
