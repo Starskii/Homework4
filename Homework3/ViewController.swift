a44689effe9fd262befa6864cea4c02abef35202
runquanye <yer@mail.gvsu.edu>
2020-02-04 16:47:39 -0500

    initial commit

--- Homework3/ViewController.swift
+++ Homework3/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var toTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+ 
+    @IBAction func calculateBtn(_ sender: Any) {
+        toTextField.text = "430 praise it"
+    }
 
-
+    @IBAction func clearBtn(_ sender: Any) {
+    }
+    @IBAction func modeBtn(_ sender: Any) {
+    }
 }
 
