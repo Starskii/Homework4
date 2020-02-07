ab1b50d17d597080bbedaf581ddbf862572dd201
runquanye <yer@mail.gvsu.edu>
2020-02-06 22:17:05 -0500

    Finished Project

---
+++ Homework3/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  Homework3
+//
+//  Created by Xcode User on 2/6/20.
+//  Copyright © 2020 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
