bdaba5cfa796391d20e73250a51884ce794255fd
runquanye <yer@mail.gvsu.edu>
2020-02-06 22:39:38 -0500

    Finished Project

--- Homework3/SettingsViewController.swift
+++ Homework3/SettingsViewController.swift
@@ -8,15 +8,83 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+protocol UnitSelection {
+    func unitSelected(fromUnit: String?, toUnit: String?)
+}
 
+class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+     var delegate: UnitSelection?
+    
+    var mode = ""
+    var fromUnitVal = "404"
+    var toUnitVal = "404"
+  
+    
+    var pickerData : [String] = [String]()
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var tLbl: UILabel!
+    @IBOutlet weak var fLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        self.picker.isHidden = true
+        fLbl.text = fromUnitVal
+        tLbl.text = toUnitVal
+        if (mode == "volume"){
+            pickerData = ["Gallons","Liters","Quarts"]
+        }else{
+            pickerData = ["Meters","Yards","Miles"]
+        }
         // Do any additional setup after loading the view.
     }
     
+    var selectedLbl = ""
+    
+    @IBAction func pickerHide(_ sender: Any) {
+ self.picker.isHidden = true
+        
+    }
+    @IBAction func toGesture(_ sender: Any) {
+        selectedLbl = "To"
+        self.picker.isHidden = false
+    }
+    @IBAction func fromGesture(_ sender: Any) {
+        selectedLbl = "From"
+        self.picker.isHidden = false
+    }
+    @IBAction func cancelBtn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func saveBtn(_ sender: Any) {
+        delegate?.unitSelected(fromUnit: fLbl.text, toUnit: tLbl.text)
+        self.dismiss(animated: true, completion: nil)
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+      
+        if(self.selectedLbl == "From"){//Don't ask too many questions
+            fLbl.text = pickerData[row]
+        }else{
+            tLbl.text = pickerData[row]
+        }
+    }
 
+
+
+    
+    
     /*
     // MARK: - Navigation
 
--- Homework3/ViewController.swift
+++ Homework3/ViewController.swift
@@ -8,41 +8,137 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UnitSelection{
+    
+    func unitSelected(fromUnit: String?, toUnit: String?) {
+        self.fromUnit.text = fromUnit!
+        self.toUnit.text = toUnit!
+    }
     
     @IBOutlet weak var toTextField: UITextField!
     @IBOutlet weak var fromTextField: UITextField!
+    @IBOutlet weak var fromUnit: UILabel!
+    @IBOutlet weak var toUnit: UILabel!
+    
+    var mode = "length"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fromUnit.text = "Yards"
+        toUnit.text = "Meters"
         // Do any additional setup after loading the view.
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
          fromTextField.endEditing(true)
          toTextField.endEditing(true)
-        
     }
- 
-
-    @IBAction func calculateBtn(_ sender: Any) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.destination.children[0] is SettingsViewController {
+            
+            let vc = segue.destination.children[0] as? SettingsViewController
+            vc?.mode = self.mode
+            vc?.fromUnitVal = self.fromUnit.text!
+            vc?.toUnitVal = self.toUnit.text!
+            vc?.delegate = self
+            
+        }
+    }
+    func getConversionValue(fromType: String, toType: String) -> Double{
         
+        var returnVal : Double = 0
+        
+        if(mode == "length"){
+            switch(fromType){
+            case toType:
+                returnVal = 1
+            case "Meters":
+                if(toType == "Yards"){
+                    returnVal = 1.09361
+                }else{
+                    returnVal = 0.000621371
+                };break
+            case "Yards":
+                if(toType == "Meters"){
+                    returnVal = 0.9144
+                }else{
+                    returnVal = 0.000568182
+                };break
+            case "Miles":
+                if(toType == "Meters"){
+                    returnVal = 1609.34
+                }else{
+                    returnVal = 1760
+                };break
+            default:
+                returnVal = 1
+            }
+        }else{
+            switch(fromType){
+            case toType:
+                returnVal = 1
+                break
+            case "Gallons":
+                if(toType == "Liters"){
+                    returnVal = 3.78541
+                }else{
+                    returnVal = 4
+                };break
+            case "Liters":
+                if(toType == "Gallons"){
+                    returnVal = 0.264172
+                }else{
+                    returnVal = 1.05669
+                };break
+            case "Quarts":
+                if(toType == "Gallons"){
+                    returnVal = 0.25
+                }else{
+                    returnVal = 0.946353
+                };break
+            default:
+                returnVal = 1
+                break//wow
+            }
+        }
+        return returnVal
+    }
+    @IBAction func settingsBtn(_ sender: Any) {
+            performSegue(withIdentifier: "seguine", sender: self)
+    }
+    
+    @IBAction func calculateBtn(_ sender: Any) {
        let  str = fromTextField.text
+        
         if let input = Double(str!){
+            var conVal = getConversionValue(fromType: fromUnit.text!, toType: toUnit.text!)
+            
+            toTextField.text = ((String)(conVal*input))
             toTextField.textColor = UIColor.black
-            let h = 0.9144
-            let output = Double(h*input)
-            toTextField.text = String(output)
         } else{
             toTextField.text = "You broke it, great."
             toTextField.textColor = UIColor.red
         }
-
-    
     }
+    
     @IBAction func clearBtn(_ sender: Any) {
         fromTextField.text = ""
         toTextField.text = ""
     }
-    @IBAction func modeBtn(_ sender: Any) {
+    
+    @IBAction func modeChange(_ sender: Any) {
+        if (mode == "length"){
+            mode = "volume"
+            fromTextField.placeholder = "Enter Volume"
+            fromUnit.text = "Gallons"
+            toUnit.text = "Liters"
+        }else{
+            mode = "length"
+            fromTextField.placeholder = "Enter Length"
+            fromUnit.text = "Yards"
+            toUnit.text = "Meters"
+        }
+        
     }
-}
+    }
+
 
