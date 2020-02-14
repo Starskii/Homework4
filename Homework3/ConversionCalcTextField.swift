8c04611d94b0f83f6ad046d41c29dbc56f991a64
runquanye <yer@mail.gvsu.edu>
2020-02-13 19:23:04 -0500

    Homework is complete

---
+++ Homework3/ConversionCalcTextField.swift
@@ -0,0 +1,21 @@
+//
+//  ConversionCalcTextField.swift
+//  Homework3
+//
+//  Created by Xcode User on 2/13/20.
+//  Copyright © 2020 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class ConversionCalcTextField: DecimalMinusTextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Homework3/ConvsionCalcLabel.swift
@@ -0,0 +1,21 @@
+//
+//  ConvsionCalcLabel.swift
+//  Homework3
+//
+//  Created by Xcode User on 2/13/20.
+//  Copyright © 2020 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class ConvsionCalcLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
